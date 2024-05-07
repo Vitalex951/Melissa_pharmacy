e616da90965e76c7d5317c4c90683247c2ab947f
Vitalex <vitalex951@gmail.com>
2024-05-07 18:01:29 +0300

    added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
+import {Metadata} from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Мелисса'
+}
 export default function RootLayout({
   children,
 }: Readonly<{
